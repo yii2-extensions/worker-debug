b4739e9a85d78a53d54d595c111367ff38ec915b
Wilmer Arambula <terabytesoftw@gmail.com>
2026-01-28 04:42:11 -0300

    fix(tests): Ensure `$stubs` is an array before overriding `microtime()` in `internal-mocker-stubs`.

--- tests/support/internal-mocker-stubs.php
+++ tests/support/internal-mocker-stubs.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 /**
  * Overrides internal-mocker stubs for specific functions.
  */
-
 $stubs = require __DIR__ . '/../../vendor/xepozz/internal-mocker/src/stubs.php';
+
+if (is_array($stubs) === false) {
+    $stubs = [];
+}
 
 $stubs['microtime'] = [
     'signatureArguments' => 'bool $as_float = false',
